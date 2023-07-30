1900cf346817e1ce9af0defb5bc8c366a4374249
Nuno Saraiva <nuno.saraiva@wacestudio.com>
2023-07-30 18:15:19 +0100

    dev

---
+++ test/Sample.js
@@ -0,0 +1,12 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+describe("Sample", function () {
+  it("Should emit winner event", async function () {
+
+  });
+});
